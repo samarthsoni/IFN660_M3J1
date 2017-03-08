ce755829f67810d9ac7ba93f8bb13fac0530bdca
Juan Ibanez <juanc5ibanez@hotmail.com>
2017-03-08 22:37:05 +1000

    Initial Commit

---
+++ GPLexTutorial/scanner.lex
@@ -0,0 +1,38 @@
+%namespace GPLexTutorial
+
+digit [0-9]
+letter [a-zA-Z]
+dot [.]
+exponentIndicator [eE]
+
+%%
+
+if                           { return (int)Tokens.IF; }
+else                         { return (int)Tokens.ELSE; }
+int                          { return (int)Tokens.INT; }
+bool                         { return (int)Tokens.BOOL; }
+
+{letter}({letter}|{digit})*  { yylval.name = yytext; return (int)Tokens.IDENT; }
+{digit}+	                 { yylval.num = int.Parse(yytext); return (int)Tokens.NUMBER; }
+{digit}*{dot}({digit})*[fFdD]?	     { yylval.floatValue =  yytext.EndsWith("f") || yytext.EndsWith("F") || yytext.EndsWith("d") || yytext.EndsWith("D")  ? float.Parse(yytext.Remove(yytext.Length-1)) : float.Parse(yytext); return (int)Tokens.FLOAT; }
+
+
+
+"="                          { return '='; }
+"+"                          { return '+'; }
+"<"                          { return '<'; }
+"("                          { return '('; }
+")"                          { return ')'; }
+"{"                          { return '{'; }
+"}"                          { return '}'; }
+";"                          { return ';'; }
+
+[ \r\n\t]                    /* skip whitespace */
+
+.                            { 
+                                 throw new Exception(
+                                     String.Format(
+                                         "unexpected character '{0}'", yytext)); 
+                             }
+
+%%
---
+++ GPlex.Tests/FloatingPointLiteralTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using GPLexTutorial;
+using GPLexTutorial.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPlex.Tests
+{
+    [TestClass]
+    public class FloatingPointLiteralTests
+    {
+        private Scanner _scanner;
+
+        public FloatingPointLiteralTests()
+        {
+            
+        }
+
+        [TestInitialize]
+        public void Initializer()
+        {
+            _scanner = new Scanner();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _scanner = null;
+        }
+
+        [TestMethod]
+        public void DigitDotDigit()
+        {
+            AssertFloatRule("1.1", 1.1f);
+        }
+
+        [TestMethod]
+        public void DotDigit()
+        {
+            AssertFloatRule(".1", 0.1f);
+        }
+
+        [TestMethod]
+        public void DotDigitSuffix()
+        {
+            AssertFloatRule(".1f", 0.1f);
+            AssertFloatRule(".1F", 0.1f);
+            AssertFloatRule(".1d", 0.1f);
+            AssertFloatRule(".1D", 0.1f);
+        }
+
+        #region Private Methods
+
+        private void AssertFloatRule(string valueToTest, float expectedValue)
+        {
+            _scanner.SetSource(new List<string>() { valueToTest });
+            Tokens token;
+            token = (Tokens)_scanner.yylex();
+            AssertIsFloat(token);
+            AssertExpectedFloatValue(expectedValue);
+        }
+
+        private void AssertIsFloat(Tokens token)
+        {
+            Assert.AreEqual(Tokens.FLOAT, token);
+        }
+
+        private void AssertExpectedFloatValue(float expectedValue)
+        {
+            Assert.AreEqual(expectedValue, _scanner.yylval.floatValue);
+        }
+
+
+        #endregion
+    }
+}
